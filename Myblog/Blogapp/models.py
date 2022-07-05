053858521bcf4f6b9b23ac5192bda7fc38224780
Kummi <bukunmiodukoya@gmail.com>
2022-07-05 11:40:07 +0100

    first commit

---
+++ Myblog/Blogapp/models.py
@@ -0,0 +1,28 @@
+from email.policy import default
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique = True )
+    slug = models.CharField(max_length = 200, unique = True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name = 'blog_posts')
+    created_on = models.DateTimeField(auto_now_add= True)
+    updated_on = models.DateTimeField(auto_now_add= True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0 )
+    image = models.ImageField(default='', upload_to='images', blank = True, null = True)
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
